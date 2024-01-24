9df9ef9e1752edb3db1cf69c3a95ca4732135da9
pola1991 <pa.albornoz@duocuc.cl>
2024-01-24 18:47:33 -0300

    uniendo componentes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu= document.querySelector('.desktop-menu');
 const mobileMenu= document.querySelector('.mobile-menu');
 const burguerIcon= document.querySelector('.menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
-const productDetail = document.querySelector('.product-detail');
+const productDetail = document.querySelector('#product-detail');
 
 
 menuEmail.addEventListener('click', ToggleDesktopMenu);
@@ -36,6 +36,7 @@ function ToggleMobileMenu() {
   
  }
 
+ 
 /* Funcion carrito de compras */
 function ToggleShoppingCart() {
   const menuMobileClosed = mobileMenu.classList.contains('inactive');
@@ -46,10 +47,11 @@ function ToggleShoppingCart() {
     mobileMenu.classList.add('inactive')
   }
 
-}
+};
 
 
 //Tipo base de datos
+
 const productsList = [];
 
 productsList.push({
@@ -73,26 +75,9 @@ productsList.push({
 
 });
 
-productsList.push({
-  name: 'Guitarra',
-  price: 900,
-  image: 'https://alveal.cl/39978-large_default/guitarra-acustica-ambar-ga3-ibanez.jpg',
-
-});
-productsList.push({
-  name: 'Guitarra',
-  price: 900,
-  image: 'https://alveal.cl/39978-large_default/guitarra-acustica-ambar-ga3-ibanez.jpg',
-
-});
-productsList.push({
-  name: 'Guitarra',
-  price: 900,
-  image: 'https://alveal.cl/39978-large_default/guitarra-acustica-ambar-ga3-ibanez.jpg',
-
-});
 
 // Creo una estructura de como quiero que se vean los productos para luego se cree esa estructura para cada producto que hay en la base de datos
+
 /*<div class="product-card">
     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
     <div class="product-info">
@@ -114,37 +99,39 @@ function renderProducts(array) {
     
     const productCardDiv = document.createElement('div');
     productCardDiv.classList.add('product-card');
-  
+
     const image = document.createElement('img');
     image.setAttribute('src', product.image);
-  
+
     const productInfo= document.createElement('div');
     productInfo.classList.add('product-info');
-  
+
     const productInfoDiv= document.createElement('div');
     const precioP= document.createElement('p');
     precioP.innerText = '$'+ product.price
-  
+
     const nameP= document.createElement('p');
     nameP.innerText = product.name;
-  
+
     productInfoDiv.appendChild(precioP);
     productInfoDiv.appendChild(nameP);
-  
+
     const figure = document.createElement('figure');
     const imgFigure= document.createElement('img');
     imgFigure.setAttribute('src', './icons/bt_add_to_cart.svg' );
-  
+
     figure.appendChild(imgFigure);
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(figure);
     productCardDiv.appendChild(image);
     productCardDiv.appendChild(productInfo);
-  
+
     cardsContainer.appendChild(productCardDiv);
     
-  
   }
 }
 
 renderProducts(productsList);
+
+
+ 
